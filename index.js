cb52418fe9f2db2fa9771b3f560ef986e9f8e6d0
osxgirl <osxgirl@mac.com>
2021-03-03 19:08:09 -0500

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ function saturdayFun(activity="roller-skate") {
 let mondayWork = function(activity = 'go to the office'){
     return `This Monday, I will ${activity}.`
 }
+
+let wrapAdjective = function(vflair= '*'){
+    return function(adj="special"){
+        return `You are ${vflair + adj  + vflair}!`
+    }
+}
+const Calculator = {
+    add: function(a,b){
+        return a + b 
+    },
+    subtract: function(a,b){
+        return a - b
+    },
+    multiply: function(a,b){
+        return a * b
+    },
+    divide: function(a,b){
+        return a / b 
+    }
+}
+
+function actionApplyer(integer, arry){
+    if (arry.length === 0){
+        return integer 
+    }
+    else {
+        arry.forEach(f => {
+            integer = f(integer)
+        });
+     return integer
+    }
+}
